44347e9b561069aef33855a8c3c2ca855fdb2a6b
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-23 08:47:11 -0600

    CH7: Creating Todos via the ListCreateAPIView
    1. "todos" endpoint updated to allow read-write.
    2. view "TodoListCreate" created.

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -10,3 +10,16 @@ class TodoList(generics.ListAPIView):
     def get_queryset(self):
         user = self.request.user
         return Todo.objects.filter(user=user).order_by('-created')
+
+class TodoListCreate(generics.ListCreateAPIView):
+    # ListAPIView requires two mandatory attributes, serializer_class and queryset.
+    # We specify TodoSerializer which we have earlier implemented.
+    serializer_class = TodoSerializer
+
+    def get_queryset(self):
+        user = self.request.user
+        return Todo.objects.filter(user=user).order_by('-created')
+
+    def perform_create(self, serializer):
+        # serializer holds a django model
+        serializer.save(user=self.request.user)
