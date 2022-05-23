48f596efda3c62a00548f843af3b6ee9e6cb6220
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-23 16:41:01 -0600

    CH11: Authentication - Sign Up
    1. "rest_framework.authtoken" app added.
    2. authentication method changed.
    3. "signup/" endpoint created.
    4. signup view created.

--- todobackend/api/views.py
+++ todobackend/api/views.py
@@ -1,6 +1,12 @@
 from rest_framework import generics, permissions
 from .serializers import TodoSerializer, TodoToggleCompleteSerializer
 from todo.models import Todo
+from django.db import IntegrityError
+from django.contrib.auth.models import User
+from rest_framework.parsers import JSONParser
+from rest_framework.authtoken.models import Token
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
 
 class TodoList(generics.ListAPIView):
     # ListAPIView requires two mandatory attributes, serializer_class and queryset.
@@ -45,3 +51,21 @@ class TodoToggleComplete(generics.UpdateAPIView):
     def perform_update(self, serializer):
         serializer.instance.completed = not(serializer.instance.completed)
         serializer.save()
+
+@csrf_exempt
+def signup(request):
+    if request.method == "POST":
+        try:
+            data = JSONParser().parse(request)
+            user = User.objects.create_user(
+                username = data['username'],
+                password = data['password']
+            )
+            user.save()
+
+            token = Token.objects.create(user=user)
+            return JsonResponse({'token':str(token)}, status=201)
+        except IntegrityError:
+            return JsonResponse(
+                {'error':'username taken, chose another username'}, status=400
+            )
