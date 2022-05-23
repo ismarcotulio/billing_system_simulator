3b380672e442d3307e1f59b15d40eea4f610ed9e
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-22 21:37:22 -0600

    CH3: Understanding the project structure
    1. back-end folder renamed.

--- todobackend/backend/urls.py
+++ todobackend/backend/urls.py
@@ -1,4 +1,4 @@
-"""todobackend URL Configuration
+"""backend URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.0/topics/http/urls/
