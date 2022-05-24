e0ef36fb8aa999ccec053fc0dc7d2bbe27f3937a
Marco Tulio Ruiz <mruizq@unah.hn>
2022-05-23 22:06:32 -0600

    CH15: Defining Our Routes
    1. routes defined.
    2. footer component added.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -48,6 +48,38 @@ function App(){
           </Nav>
         </div>
       </Navbar>
+
+      <div className="container mt-4">
+        <Switch>
+          <Route exact path={["/", "/todos"]} render={(props)=>
+            <TodosList {...props} token={token} />
+          }>
+          </Route>
+          <Route path="/todos/create" render={(props)=>
+            <AddTodo {...props} token={token} />
+          }>
+          </Route>
+          <Route path="/todos/:id" render={(props)=>
+            <AddTodo {...props} token={token}/>
+          }>
+          </Route>
+          <Route path="/login" render={(props)=>
+            <Login {...props} login={login}/>
+          }>
+          </Route>
+          <Route path="/signup" render={(props)=>
+            <Signup {...props} signup={signup} />
+          }>
+          </Route>
+        </Switch>
+      </div>
+      <footer className="text-center text-lg-start bg-light text-muted mt-4">
+        <div className="text-center p-4">
+          Copyright - <a target="_blank" className="text-reset fw-bold text-decoration-none" href="https://twitter.com/MarcoTulio_Ruiz">
+            Marco Tulio Ruiz
+          </a>
+        </div>
+      </footer>
     </div>
   );
 }
